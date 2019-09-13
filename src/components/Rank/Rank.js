73df667d75373b2ca1abe5697ef55ca9c95d85ce
namanmanchanda09 <naman.mohan09@outlook.com>
2019-09-14 00:24:13 +0530

    added all the components

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,10 @@ const Rank = () =>{
     return(
         <div>
             <div className='white f3'>
-                
+                {'Naman, your current rank is...'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
             </div>
         </div>
     );
